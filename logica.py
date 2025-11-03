7753f256ef428778d4920afd2b546bf185c148ff
Santino Dalla Torre <santinodallatorre2501@gmail.com>
2025-11-03 17:30:29 -0300

    Esqueleto lógica

---
+++ logica.py
@@ -0,0 +1,12 @@
+def lista_paises():
+    pass
+def mostrar_estadísticas():
+    pass
+def ordenar_paises():
+    pass
+def filtrar_pais():
+    pass
+def buscar_pais():
+    pass
+def menu():
+    pass
