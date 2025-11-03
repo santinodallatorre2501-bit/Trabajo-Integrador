676064d72ba42769080114cef5a55c6ab44e5c12
valentello1 <vtellofotografia@gmail.com>
2025-11-03 17:56:24 -0300

    mejora de menú
    Adición de todas las funciones y del menú interactuable

--- logica.py
+++ logica.py
@@ -1,12 +1,51 @@
-def lista_paises():
+def cargar_csv():
     pass
-def mostrar_estadísticas():
+
+def paises_por_nombre():
     pass
-def ordenar_paises():
+def pais_por_continente():
     pass
-def filtrar_pais():
+def pais_por_poblacion():
     pass
-def buscar_pais():
+def pais_por_superficie():
     pass
+def ord_paises_por_nombre():
+    pass
+def ord_paises_por_poblacion():
+    pass
+def ord_paises_por_superficie():
+    pass
+def mostar_mayor_poblacion():
+    pass
+def mostrar_menor_poblacion():
+    pass
+def mostrar_promedio_poblacion():
+    pass
+def mostrar_promedio_superficie():
+    pass
+def mostrar_cantidad_paises_continente():
+    pass
+
+
 def menu():
-    pass
+    opcion = 0
+    print("""
+--- BIENVENIDO AL MENÚ DE OPCIONES ---
+1:  Buscar país por nombre
+2:  Filtrar país por continente
+3:  Filtrar país por rango de población
+4:  Filtrar país por rango de superficie
+5:  Ordenar países por nombre
+6:  Ordenar países por población
+7:  Ordenar países por superficie
+8:  Mostrar país con mayor población
+9:  Mostrar país con menor población
+10: Mostrar promedio de población
+11: Mostrar promedio de superficie
+12: Mostrar cantidad de países por continente
+0:  Salir
+""")
+    opcion = int(input("Ingrese la opcion: "))
+
+cargar_csv()
+menu()
